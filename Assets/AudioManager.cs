c0b016ce6f23a2d24ca6fd3d4649215d106eddd3
Acewwww <159698781+Acewwww@users.noreply.github.com>
2024-02-11 23:02:36 +0700

    Initial commit

---
+++ Assets/AudioManager.cs
@@ -0,0 +1,15 @@
+
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+   [SerializeField] AudioManager musicSource;
+   [SerializeField] AudioManager SFXSource;
+   public AudioClip background;
+   public AudioClip benar;
+   public AudioClip salah;
+   public AudioClip gameOver;
+   public AudioClip klik;
+
+
+}
